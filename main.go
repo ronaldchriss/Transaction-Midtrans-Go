5c5570d8c0cdfff068f5b9e416919584d1ccf960
Ronald Christian <ronchrissoe@gmail.com>
2022-01-09 13:48:34 +0700

    CORS

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -41,6 +42,7 @@ func main() {
 	transHandler := handler.NewTransactionHandler(TransService)
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.Static("/images/", "./images")
 	api := router.Group("/api/v1")
 
